bb6d60759971770fac6fa3fe500d84ac5daac0d1
Jannis R <mail@jannisr.de>
2018-07-15 20:51:38 +0200

    respect err.statusCode, 0.5.2

--- handle-errors.js
+++ handle-errors.js
@@ -4,7 +4,7 @@ const handleErrors = (err, req, res, next) => {
 	if (process.env.NODE_ENV === 'dev') console.error(err)
 	if (res.headersSent) return next()
 
-	let msg = err.message, code = null
+	let msg = err.message, code = err.statusCode || null
 	if (err.isHafasError) {
 		msg = 'VBB error: ' + msg
 		code = 502
