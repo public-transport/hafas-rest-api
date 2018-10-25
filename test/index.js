890f5514f78791226550fe92ffc3f027b495e193
Jannis R <mail@jannisr.de>
2018-10-25 21:35:00 +0200

    WIP

--- test/index.js
+++ test/index.js
@@ -11,4 +11,35 @@ const {
 
 const test = tapePromise(tape)
 
+test('/station/:id', async(t) => {
+	const {fetch, stop} = await createTestApi({
+		station: (id) => {
+			if (id !== stationA.id) throw new Error('station() called with invalid ID')
+			return Promise.resolve(stationA)
+		}
+	})
+
+	const {data} = await fetch('/stations/' + stationA.id)
+	t.deepEqual(data, stationA)
+
+	await stop()
+	t.end()
+})
+
+test('/station/:id', async(t) => {
+	const {fetch, stop} = await createTestApi({
+		nearby: (loc) => {
+			if (loc.latitude !== 123) throw new Error('nearby() called with invalid latitude')
+			if (loc.longitude !== 321) throw new Error('nearby() called with invalid longitude')
+			return Promise.resolve([stationA, stationB])
+		}
+	})
+
+	const {data} = await fetch('/stations/nearby?latitude=123&longitude=321')
+	t.deepEqual(data, [stationA, stationB])
+
+	await stop()
+	t.end()
+})
+
 // todo
