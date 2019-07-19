9275516a6220767c6aea6232f854922975a0a616
Jannis R <mail@jannisr.de>
2019-07-19 23:50:27 +0200

    /events: check accept header, prepare for GTFS-RT

--- routes/events.js
+++ routes/events.js
@@ -4,6 +4,8 @@ const createServer = require('hafas-monitor-trips-server')
 const SSE = require('ssestream').default
 const {parseNumber} = require('../lib/parse')
 
+const EVENT_STREAM = 'text/event-stream'
+
 const EVENTS = [
 	'trip', 'new-trip', 'trip-obsolete',
 	'stopover',
@@ -17,7 +19,26 @@ const err400 = (msg) => {
 	return err
 }
 
+const sseWriter = (req, res, evNames) => {
+	const sse = new SSE(req)
+	sse.pipe(res)
+
+	const createHandler = (evName) => {
+		const sendEvent = (data) => {
+			sse.write({event: evName, data})
+		}
+		return [evName, sendEvent]
+	}
+	return [
+		...evNames.map(createHandler),
+		['error', (err) => {
+			sse.write({event: 'error', data: err.message || (err + '')})
+		}]
+	]
+}
+
 const createEventsRoute = (hafas, config) => {
+routes/monitor.js
 	const monitor = createServer(hafas)
 
 	const events = (req, res, next) => {
@@ -35,29 +56,31 @@ const createEventsRoute = (hafas, config) => {
 		const events = EVENTS.filter(evName => req.query[evName] === 'true')
 		if (events.length === 0) return next(err400('0 events selected'))
 
-		const sse = new SSE(req)
-		sse.pipe(res)
-		const handlers = events.map((evName) => {
-			const sendEvent = (data) => {
+		if (req.accepts(EVENT_STREAM) !== EVENT_STREAM) {
+			return next(err400(`only ${EVENT_STREAM}`))
+		}
+
+		const handlers = sseWriter(req, res, events)
+		const sub = monitor.subscribe(bbox)
+		handlers
+		.filter(([evName]) => evName !== 'error')
+		.forEach(([evName, handler]) => {
+			sub.on(evName, (data) => {
 				logger.debug({event: evName, data, bbox})
-				sse.write({event: evName, data})
-			}
-			return [evName, sendEvent]
+				handler(data)
+			})
 		})
 
-		const sub = monitor.subscribe(bbox)
-		for (const [evName, handler] of handlers) {
-			sub.on(evName, handler)
-		}
 		logger.info({bbox}, 'starting monitor')
 		res.once('close', () => {
 			logger.info({bbox}, 'stopping monitor')
 			sub.destroy()
 		})
 
+		const onError = handlers.find(([evName]) => evName === 'error')[1]
 		sub.on('error', (err) => {
 			logger.error({bbox}, err)
-			sse.write({event: 'error', data: err.message || (err + '')})
+			onError(err)
 		})
 	}
 
