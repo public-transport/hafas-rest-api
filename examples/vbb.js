4e5907e66b8c6e07390a14946c42cf3092e17579
Jannis R <mail@jannisr.de>
2022-03-23 11:39:31 +0100

    VBB example: add caching via cached-hafas-client 📝

--- examples/vbb.js
+++ examples/vbb.js
@@ -3,6 +3,9 @@
 const path = require('path')
 const createHafas = require('hafas-client')
 const vbbProfile = require('hafas-client/p/vbb')
+const Redis = require('ioredis')
+const withCaching = require('cached-hafas-client')
+const createRedisStore = require('cached-hafas-client/stores/redis')
 
 const createApi = require('..')
 
@@ -24,7 +27,21 @@ const config = {
 	}
 }
 
-const hafas = createHafas(vbbProfile, 'hafas-rest-api-example')
+const rawHafas = createHafas(vbbProfile, 'hafas-rest-api-example')
+
+let hafas = rawHafas
+if (process.env.REDIS_URL) {
+	const opts = {}
+	const url = new URL(process.env.REDIS_URL)
+	opts.host = url.hostname || 'localhost'
+	opts.port = url.port || '6379'
+	if (url.password) opts.password = url.password
+	if (url.pathname && url.pathname.length > 1) {
+		opts.db = parseInt(url.pathname.slice(1))
+	}
+	const redis = new Redis(opts)
+	hafas = withCaching(rawHafas, createRedisStore(redis))
+}
 
 const api = createApi(hafas, config, () => {})
 
